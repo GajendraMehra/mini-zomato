e859acf0a5fa20ede3c30f25538ea28e47740cc3
Varinder Singh <varinderrana.vr@gmail.com>
2021-02-11 22:19:38 +0530

    Restaurats api request added

--- src/components/restaurants/index.js
+++ src/components/restaurants/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
 import App from '../../App'
+import {getRestaurants} from '../../request/'
+
 function Restaurants() {
+    const  fetchRestaurant=async ()=>{
+console.log(location);
+
+        await getRestaurants(location.id).then((data)=>{
+         
+          console.log(data.data.collections);
+          
+        })
+         
+      }
+    const [location, setLocation] = useState(null)
+    useEffect(() => {
+        // alert()
+        return () => {
+         
+            (location)&&fetchRestaurant()
+         
+        }
+      }, [location])
     return (
         <div>
-           <App/>
+           <App data={(locDetail)=>{
+            setLocation(locDetail)
+            
+             console.log(locDetail);
+            
+           }}></App>
         </div>
     )
 }
--- src/request/index.js
+++ src/request/index.js
@@ -16,3 +16,20 @@ export const trendingMeal =async (id) => {
       
       
   };
+
+
+  export const getRestaurants =async (id) => {
+    var url = `https://developers.zomato.com/api/v2.1/collections?city_id=${id}`;
+
+   return  await axios
+      .get(url, {
+        headers: {
+          "Content-Type": "application/json",
+          "user-key": "0110bd60e6845c9aa66418529017dab6",
+        },
+      })
+  
+      
+      
+      
+  };
