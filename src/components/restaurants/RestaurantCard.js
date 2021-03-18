e37250cf8e1da09628b953a863f3b9e829ec695c
gajendramehra <gajendramehra14rgnv@gmail.com>
2021-03-18 20:35:46 +0530

    Restaurants Modal added

--- src/components/restaurants/RestaurantCard.js
+++ src/components/restaurants/RestaurantCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Button from '@material-ui/core/Button';
@@ -16,10 +16,14 @@ import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import Grid from '@material-ui/core/Grid';
-
+import Modal from "@material-ui/core/Modal";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
@@ -46,33 +50,41 @@ const useStyles = makeStyles((theme) => ({
 export default function RestaurantCard({
   restaurant
 }) {
-  {console.log('restaurant')}
-  {console.log(restaurant)}
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [fullWidth, setFullWidth] = useState(true);
+  const [maxWidth, setMaxWidth] = useState('lg');
+  const [currentrestaurant, setCurrentrestaurant] = useState(null)
+
+  const handleClickOpen = (res) => {
+    setCurrentrestaurant(res)
+    console.log(currentrestaurant);
+    
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+
+    setOpen(false);
   };
 
   return (
+    <>
     <Card className={classes.root} style={{
       'margin':"10px",
       "width":"33%"
     }}>
       <CardHeader
-        avatar={
-            <Avatar aria-label="recipe" className={classes.avatar}>
-             {restaurant.name}
-            </Avatar>
-        }
+       
         action={
           <IconButton aria-label="settings">
             <MoreVertIcon />
           </IconButton>
         }
         title={restaurant.name}
-        subheader={`User Reviews : `+restaurant.all_reviews_count}
+        subheader={`Rating : `+restaurant.user_rating.aggregate_rating}
       />
       <CardMedia
         className={classes.media}
@@ -86,7 +98,10 @@ export default function RestaurantCard({
       </CardContent>
       <CardActions >
       <Grid container justify="flex-start">
-      <Button href=  {restaurant.url} variant="outlined"  size="small" color="primary">
+      <Button onClick={()=>{
+     
+        handleClickOpen(restaurant)
+      }} variant="outlined"  size="small" color="primary">
       View Detail
      </Button>
       </Grid>
@@ -106,5 +121,28 @@ export default function RestaurantCard({
       
       
     </Card>
+ 
+
+    <Dialog
+    fullWidth={fullWidth}
+    maxWidth={maxWidth}
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="max-width-dialog-title"
+  >
+    <DialogTitle id="max-width-dialog-title">Optional sizes</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+      {currentrestaurant&&currentrestaurant.name}
+      </DialogContentText>
+     
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleClose} color="primary">
+        Close
+      </Button>
+    </DialogActions>
+  </Dialog>
+    </>
   );
 }
